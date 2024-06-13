3f275453e6904360c5687cbd74ba0879048ca1d2
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-06-14 00:30:13 +0300

    Update MovingWrapper.jsx

--- src/components/MovingWrapper/MovingWrapper.jsx
+++ src/components/MovingWrapper/MovingWrapper.jsx
@@ -52,17 +52,21 @@ export const MovingWrapper = () => {
 
   return (
     <div
-      className="flex items-center gap-[16px] bg-lightColor rounded-[20px] p-[32px] absolute top-0 left-[50%] w-[284px] z-[-1] overflow-hidden animate-moveBox"
+      className="flex items-center gap-[12px] lg:gap-[16px] bg-lightColor rounded-[20px] p-[14px] md:p-[24px] lg:p-[32px] absolute bottom-[-100%] left-[-12px] md:bottom-[-130%] md:left-[-20px] lg:top-0 lg:bottom-auto lg:left-[50%] w-[180px] md:w-[254px] lg:w-[284px] z-[1] lg:z-[-1] overflow-hidden animate-moveBox"
       onAnimationStart={handleAnimationStart}
     >
-      <div className="flex justify-center items-center w-[54px] h-[54px] rounded-[13px] bg-accentColor">
-        <Icon id="check" className="fill-lightColor lg:size-[30px]" size="30" />
+      <div className="flex justify-center items-center w-[38px] h-[38px] md:w-[46px] md:h-[46px] lg:w-[54px] lg:h-[54px] rounded-[13px] bg-accentColor">
+        <Icon
+          id="check"
+          className="fill-lightColor mb:size-[28px] lg:size-[30px]"
+          size="18"
+        />
       </div>
       <div>
-        <p className="font-normal text-[16px] leading-[125%] text-[#11101c7f] mb-[4px]">
+        <p className="font-normal text-[10px] md:text-[16px] leading-[125%] text-[#11101c7f] mb-[4px]">
           Experienced nannies
         </p>
-        <p className="font-bold text-[24px] leading-[125%] text-darkColor">
+        <p className="font-bold text-[18px] md:text-[24px] leading-[125%] text-darkColor">
           {randomSum}
         </p>
       </div>
