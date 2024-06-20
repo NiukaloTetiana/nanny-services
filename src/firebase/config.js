c59832ed886c3e4133694afbb5dc57acdb702ae4
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-06-20 21:16:31 +0300

    add url

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: import.meta.env.API_KEY,
   authDomain: import.meta.env.AUTH_DOMAIN,
+  databaseURL: import.meta.env.DATABASE_URL,
   projectId: import.meta.env.PROJECT_ID,
   storageBucket: import.meta.env.STORAGE_BUCKET,
   messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
@@ -13,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const database = getDatabase(app);
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { Slide, ToastContainer } from "react-toastify";
 
 import App from "./components/App";
 
@@ -10,6 +10,11 @@ import "react-toastify/dist/ReactToastify.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
-    <ToastContainer theme="dark" style={{ zIndex: 99999 }} autoClose={2000} />
+    <ToastContainer
+      theme="dark"
+      style={{ zIndex: 99999 }}
+      autoClose={2000}
+      transition={Slide}
+    />
   </BrowserRouter>
 );
